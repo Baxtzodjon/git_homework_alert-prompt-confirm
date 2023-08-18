fd3aa12c1bb831f39c1697b63d723b57cb7f572d
Rasulov Baxtzod <141494256+Baxtzodjon@users.noreply.github.com>
2023-08-18 14:13:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let name = 'Alex'
+let money = 10000
+let account = 7777
+
+
+let a1 = +prompt('Как вас зовут?')
+
+if (name) {
+    alert('Alex')
+    prompt('Номер счета?')
+    alert('7777')
+} else {
+    alert('Пользователь не найден, досвидули')
+}
+
+if (account) {
+    prompt('Сколько обналичить?')
+    alert('10000')
+} else {
+    alert('Пользователь не найден, досвидули')
+}
+
+let a2 = +prompt('Сколько снял?')
+alert('10000')
+
+if (money) {
+    alert('Сколько осталось?')
+    alert('не осталось')
+}
+
+
+
+
